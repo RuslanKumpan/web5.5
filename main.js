10b635025ea015fbcc718dc1f8b8fdee19ac282f
RuslanKumpan <90194655+RuslanKumpan@users.noreply.github.com>
2022-02-23 22:17:36 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
-function calc() {
-    let cost = document.getElementsByName("cost");
-    let kol = document.getElementsByName("kol");
-    let result = document.getElementById("result");
-    let re = /\D/;
-    if ((cost[0].value.match(re) || kol[0].value.match(re)) === null)
-        result.innerHTML = ("Стоимость вашего заказа: " + parseFloat(cost[0].value, 10) * parseInt(kol[0].value, 10));
-    else result.innerHTML = "Ошибка! Неверный формат чисел";
-    return false;
+function price()
+{
+    var a = Number(document.getElementById("quantity").value);
+    var b = Number(document.getElementById("price").value);
+    var d = Number(document.getElementById("quantity1").value);
+    if(a == "" || b == "" || d == "")
+    {
+        alert("Ошибка! Неверный формат чисел:");
+    }
+    else
+        if (!(/^(0|[1-9][0-9]*)$/).test(a) || !(/^(0|[1-9][0-9]*)$/).test(b))
+        {
+            alert("Ошибка! Неверный формат чисел:");
+        }
+        else
+        {
+            var c = a * b + d;
+            document.getElementById("pay").innerHTML = "Стоимость топлива составляет: " + c;
+        }
+    document.addEventListener("DOMContentLoaded", function(){
+        console.log("DOMContent Loaded");
+        let h = document.getElementById("button");
+        h.addEventListener("click", price);
+    });
 }
-window.addEventListener('DOMContentLoaded', function(event) {
-    let btn = document.getElementById("button1");
-    btn.addEventListener("click", getResult);
-});
